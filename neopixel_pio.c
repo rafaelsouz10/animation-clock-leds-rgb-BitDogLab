89231c89983c917a83ed7d034b2e00297226833c
Rafael Souza <rafaelsouz10@gmail.com>
2025-01-25 22:24:45 -0300

    adicionado diagrama.json

---
+++ neopixel_pio.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "include/matriz_led.h"
+#include "include/animacao_relogio_teste.h"
+
+int main() {
+
+  // Inicializa entradas e saídas.
+  stdio_init_all();
+
+  // Inicializa matriz de LEDs NeoPixel.
+  npInit(LED_PIN);
+
+  // Inicializa o buzzer
+  init_buzzer();
+  
+  while (true) {
+
+    //animação recebe parametro delay
+    clock_animation(500);
+  }
+}
